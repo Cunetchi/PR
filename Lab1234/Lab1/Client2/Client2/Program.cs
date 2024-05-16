7c43d30970c4aa0cadde81f851fea73b8d71d2b0
Cunetchi <mihailkunetchi@gmail.com>
2024-05-16 10:03:36 +0300

    LabPR2U

---
+++ Lab1234/Lab1/Client2/Client2/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+IPAddress serverIp = IPAddress.Parse("127.0.0.1");
+int serverPort = 9001;
+
+Thread.Sleep(3000);
+
+while (true)
+{
+    Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+    client.Connect(new IPEndPoint(serverIp, serverPort));
+    Console.WriteLine("Connected to server");
+    
+    Console.WriteLine("Enter some text to send to server");
+    string text = Console.ReadLine() ?? "";
+
+    byte[] bytesData = Encoding.UTF8.GetBytes(text);
+
+    client.Send(bytesData);
+
+    string receivedText = "";
+
+    do
+    {
+        byte[] receivedBuffer = new byte[1024];
+        int bytesReceived = client.Receive(receivedBuffer);
+
+        receivedText += Encoding.UTF8.GetString(receivedBuffer, 0, bytesReceived);
+    } while (client.Available > 0);
+
+    Console.WriteLine($"Server: {receivedText}\n");
+
+    client.Shutdown(SocketShutdown.Both);
+    client.Close();
+}
---
+++ Lab1234/Lab1/Server/ConsoleApp1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+        serverSocket.Bind(new IPEndPoint(ipAddress, 9001));
+
+        serverSocket.Listen(5);
+
+        int connectionCount = 0;
+
+        while (true)
+        {
+            Console.WriteLine("Waiting for a connection...");
+
+            Socket connection = await serverSocket.AcceptAsync();
+            connectionCount++;
+
+            Console.WriteLine($"Connection {connectionCount} accepted");
+            Console.WriteLine(connection.RemoteEndPoint);
+
+            // Start a new thread to handle the connection asynchronously
+            _ = Task.Run(() => HandleConnection(connection, connectionCount));
+        }
+    }
+
+    static void HandleConnection(Socket connection, int connectionCount)
+    {
+        try
+        {
+            string text = "";
+
+            do
+            {
+                byte[] buffer = new byte[1024];
+                int bytesReceived = connection.Receive(buffer);
+
+                text += Encoding.UTF8.GetString(buffer, 0, bytesReceived);
+            } while (connection.Available > 0);
+
+            Console.WriteLine($"Received from Connection {connectionCount}: {text}");
+
+            byte[] bytesData = Encoding.UTF8.GetBytes("Server received your message");
+            connection.Send(bytesData);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error handling Connection {connectionCount}: {ex.Message}");
+        }
+        finally
+        {
+            connection.Close();
+        }
+    }
+}
